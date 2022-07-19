fdd6a7fc18ca99c9f4423268e41175d624b2907f
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-07-19 14:43:26 +0100

    adding getProducts function to execute get request to fake store API

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query'; 
-import App from './App';
+import App from './Components/App/App';
 
+// creating a client 
 const client = new QueryClient();
 
 ReactDOM.render( 
-
+// providing client to App 
 <QueryClientProvider client={client}>
     <App /> 
 </QueryClientProvider>  
  
-  
   , document.getElementById('root'))
 
   
