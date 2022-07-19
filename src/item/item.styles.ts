9ae5d5660c2eeb36aeca3e6dcc447befc9c2c5e5
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-07-19 16:39:20 +0100

    adding css for item styled wrapper div, hover feature added to cards/items

--- src/item/item.styles.ts
+++ src/item/item.styles.ts
@@ -5,8 +5,26 @@ display: flex;
 justify-content: space-between; 
 flex-direction: column; 
 width: 100%; 
-border: 1px solid blue; 
+border: 1px solid lightblue; 
+border-radius: 20px; 
+height: 100% ;
 
-button
+&:hover {
+    background-color: lightgrey;
+}
 
+button {
+    border-radius: 0 0 20px 20px; 
+}
+
+img {
+    max-height: 250px; 
+    object-fit: cover;
+    border-radius: 20px 20px 0 0;
+    margin : 0.5px
+}
+
+p {
+    margin: 3px
+}
 `; 
